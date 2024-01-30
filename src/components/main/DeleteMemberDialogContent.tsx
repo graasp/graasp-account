720899f0b654ddb1e9266583f768a15ddafe2fa1
LinaYahya <linaebe0@gmail.com>
2024-01-30 10:47:52 +0100

    fix: change delete confirmation vlaue translation key

--- src/components/main/DeleteMemberDialogContent.tsx
+++ src/components/main/DeleteMemberDialogContent.tsx
@@ -27,7 +27,7 @@ const DeleteMemberDialogContent = ({ id, closeModal }: Props): JSX.Element => {
 
   const { mutate: deleteMember } = mutations.useDeleteMember();
 
-  const confirmationDeleteTextToCompare = translateAccount('DELETE');
+  const confirmationDeleteTextToCompare = translateAccount('DELETE_CONFIRMATION_VALUE');
 
   return (
     <>
