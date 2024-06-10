85c480015d18d8a21eea4fa888306b31e0e075a6
spaenleh <spaenleh@gmail.com>
2024-06-10 08:25:51 +0200

    fix: remove console.log

--- src/components/main/MemberCard.tsx
+++ src/components/main/MemberCard.tsx
@@ -74,7 +74,6 @@ const MemberCard = (): JSX.Element | null => {
   }
   const onThumbnailUpload = (payload: { avatar: Blob }) => {
     const { avatar } = payload;
-    console.log(avatar.size);
     if (!avatar) {
       return;
     }
