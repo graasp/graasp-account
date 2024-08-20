2be55a0407daefe7a33f23e5d68524c14c7e686e
mariembencheikh <mariembencheikh71@gmail.com>
2024-08-20 13:48:30 +0200

    fix: fix bug selecting 100 items per page

--- src/components/main/MemberStorageFiles.tsx
+++ src/components/main/MemberStorageFiles.tsx
@@ -117,6 +117,7 @@ const MemberStorageFiles = (): JSX.Element | null => {
           onPageChange={(event, newPage) => handlePageChange(event, newPage)}
           rowsPerPage={pagination.pageSize}
           onRowsPerPageChange={handlePageSizeChange}
+          rowsPerPageOptions={[10, 25, 50]} // Custom rows per page options
         />
       </>
     );
