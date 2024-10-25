722a281ffad513d16989f30d50d70bd6dc06b810
spaenleh <spaenleh@gmail.com>
2024-10-25 08:36:03 +0200

    fix: type issues

--- src/components/ui/ButtonLink.tsx
+++ src/components/ui/ButtonLink.tsx
@@ -4,6 +4,7 @@ import { Button, ButtonProps } from '@mui/material';
 
 import { LinkComponent, createLink } from '@tanstack/react-router';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface MUILinkProps extends Omit<ButtonProps, 'href'> {
   // Add any additional props you want to pass to the button
 }
