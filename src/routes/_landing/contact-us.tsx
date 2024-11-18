4b7270f78d3d0cbff9368f9be95176788bb30d3e
spaenleh <spaenleh@gmail.com>
2024-11-18 16:02:02 +0100

    fix: styling

--- src/routes/_landing/contact-us.tsx
+++ src/routes/_landing/contact-us.tsx
@@ -1,11 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
-import { WorkInProgress } from '@/components/WorkInProgress'
+import { WorkInProgress } from '@/components/WorkInProgress';
 
 export const Route = createFileRoute('/_landing/contact-us')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <WorkInProgress />
+  return <WorkInProgress />;
 }
