546f3171d660314dccd1f852c4d9f68a04c01444
spaenleh <spaenleh@gmail.com>
2024-11-18 13:04:16 +0100

    feat: add terms page

---
+++ src/routes/_landing/about-us.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+import { WorkInProgress } from '@/components/WorkInProgress'
+
+export const Route = createFileRoute('/_landing/about-us')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <WorkInProgress />
+}
---
+++ src/routes/_landing/features.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+import { WorkInProgress } from '@/components/WorkInProgress'
+
+export const Route = createFileRoute('/_landing/features')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <WorkInProgress />
+}
