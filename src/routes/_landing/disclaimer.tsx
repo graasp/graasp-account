156c3ea1725186748a4d7f03793233b05142193c
spaenleh <spaenleh@gmail.com>
2024-11-18 13:09:36 +0100

    fix: add disclaimer

--- src/routes/_landing/disclaimer.tsx
+++ src/routes/_landing/disclaimer.tsx
@@ -1,11 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useTranslation } from 'react-i18next';
 
-import { WorkInProgress } from '@/components/WorkInProgress'
+import { Stack, Typography } from '@mui/material';
+
+import { createFileRoute } from '@tanstack/react-router';
+
+import { NS } from '@/config/constants';
 
 export const Route = createFileRoute('/_landing/disclaimer')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <WorkInProgress />
+  const { t } = useTranslation(NS.Landing, { keyPrefix: 'DISCLAIMER' });
+  return (
+    <Stack direction="column" maxWidth="md" gap={6}>
+      <Typography variant="h1" color="primary">
+        {t('TITLE')}
+      </Typography>
+      <Typography>{t('TEXT')}</Typography>
+    </Stack>
+  );
 }
