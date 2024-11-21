7e3b9e7c9fc7dc4808890f33835e4f904c2466ee
spaenleh <spaenleh@gmail.com>
2024-11-21 13:35:03 +0100

    fix: rename file

--- src/modules/auth/components/requestPasswordReset/RequestPasswordReset.tsx
+++ src/modules/auth/components/requestPasswordReset/RequestPasswordReset.tsx
@@ -21,7 +21,7 @@ import { HELP_EMAIL } from '~auth/constants';
 import { AUTH } from '~auth/langs';
 
 import { useRecaptcha } from '../../context/RecaptchaContext';
-import { EmailAdornment } from '../common/Adornments';
+import { EmailAdornment } from '../common/adornments';
 import { CenteredContent } from '../layout/CenteredContent';
 import { DialogHeader } from '../layout/DialogHeader';
 
