c283e6b44f50d6d26171cc65871f3ecae71708d9
spaenleh <spaenleh@gmail.com>
2024-11-26 16:19:10 +0100

    fix: add unit tests

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -15,7 +15,10 @@ i18n
       loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
     // specify which languages are supported
-    supportedLngs: ['en', 'fr', 'de', 'it', 'es', 'ar'],
+    // 1. prefers exact match across all supported langs
+    // 2. prefers lang without locale
+    // 3. uses fallback
+    supportedLngs: ['fr', 'de', 'it', 'es', 'ar', 'en'],
     // options for the language detector
     detection: {
       order: ['localStorage', 'navigator', 'querystring'],
