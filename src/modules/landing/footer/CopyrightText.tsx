da4a4929f1d516ef85c5b796a5f0be966c3c1200
spaenleh <spaenleh@gmail.com>
2024-12-11 10:08:50 +0100

    feat: add preview component

---
+++ src/modules/landing/footer/CopyrightText.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+import { Typography } from '@mui/material';
+
+import { usePreviewMode } from '~landing/preview/PreviewModeContext';
+
+export function CopyrightText() {
+  const [clickCounter, setClickCounter] = useState(0);
+  const { togglePreview, isEnabled: isPreviewEnabled } = usePreviewMode();
+
+  const handleClick = () => {
+    if (clickCounter === 2) {
+      togglePreview();
+      // reset counter
+      setClickCounter(0);
+    } else {
+      setClickCounter((s) => s + 1);
+    }
+  };
+  return (
+    <Typography textAlign="center" variant="note" onClick={handleClick}>
+      &copy; Graasp 2014 - {new Date().getFullYear()}
+      {isPreviewEnabled ? ' (preview)' : ''}
+    </Typography>
+  );
+}
